fcf201de20c311f915ee19c80b20315e233ea72e
ibireme <ibireme@gmail.com>
2014-11-04 12:14:26 +0800

    Release 0.2.0

--- YYKit/NSObject+YYAddForARC.m
+++ YYKit/NSObject+YYAddForARC.m
@@ -7,9 +7,9 @@
 //
 
 #import "NSObject+YYAddForARC.h"
-#import "YYKitMacro.h"
 
-SYNTH_DUMMY_CLASS(NSObject_YYAddForARC)
+@interface NSObject_YYAddForARC : NSObject @end
+@implementation NSObject_YYAddForARC @end
 
 #if __has_feature(objc_arc)
 #error This file must be compiled without ARC. Specify the -fno-objc-arc flag to this file.
