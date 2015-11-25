efc8a836a15f5e8ef899172905cb77aa9664fdfc
lifeng <lfeng@datapush.cn>
2015-11-25 19:39:03 +0800

    fix spelling

--- YYKit/Base/Foundation/NSArray+YYAdd.m
+++ YYKit/Base/Foundation/NSArray+YYAdd.m
@@ -27,7 +27,7 @@ YYSYNTH_DUMMY_CLASS(NSArray_YYAdd)
 
 + (NSArray *)arrayWithPlistString:(NSString *)plist {
     if (!plist) return nil;
-    NSData* data = [plist dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *data = [plist dataUsingEncoding:NSUTF8StringEncoding];
     return [self arrayWithPlistData:data];
 }
 
