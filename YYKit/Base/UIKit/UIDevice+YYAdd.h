f0e1de007e2d9cd81abc8b606e723768996c0f0f
ibireme <ibireme@gmail.com>
2015-11-28 19:35:04 +0800

    add a method to get network traffic :#12

--- YYKit/Base/UIKit/UIDevice+YYAdd.h
+++ YYKit/Base/UIKit/UIDevice+YYAdd.h
@@ -61,6 +61,55 @@
 @property (nonatomic, readonly) NSString *ipAddressCell;
 
 
+/**
+ Network traffic type:
+ 
+ WWAN: Wireless Wide Area Network.
+ For example: 3G/4G.
+ 
+ WIFI: Wi-Fi.
+ 
+ AWDL: Apple Wireless Direct Link (peer-to-peer connection).
+ For exmaple: AirDrop, AirPlay, GameKit.
+ */
+typedef NS_OPTIONS(NSUInteger, YYNetworkTrafficType) {
+    YYNetworkTrafficTypeWWANSent     = 1 << 0,
+    YYNetworkTrafficTypeWWANReceived = 1 << 1,
+    YYNetworkTrafficTypeWIFISent     = 1 << 2,
+    YYNetworkTrafficTypeWIFIReceived = 1 << 3,
+    YYNetworkTrafficTypeAWDLSent     = 1 << 4,
+    YYNetworkTrafficTypeAWDLReceived = 1 << 5,
+    
+    YYNetworkTrafficTypeWWAN = YYNetworkTrafficTypeWWANSent | YYNetworkTrafficTypeWWANReceived,
+    YYNetworkTrafficTypeWIFI = YYNetworkTrafficTypeWIFISent | YYNetworkTrafficTypeWIFIReceived,
+    YYNetworkTrafficTypeAWDL = YYNetworkTrafficTypeAWDLSent | YYNetworkTrafficTypeAWDLReceived,
+    
+    YYNetworkTrafficTypeALL = YYNetworkTrafficTypeWWAN |
+                              YYNetworkTrafficTypeWIFI |
+                              YYNetworkTrafficTypeAWDL,
+};
+
+/**
+ Get device network traffic bytes.
+ 
+ @discussion This is a counter since the device's last boot time.
+ Usage:
+ 
+     uint64_t bytes = [[UIDevice currentDevice] getNetworkTrafficBytes:YYNetworkTrafficTypeALL];
+     NSTimeInterval time = CACurrentMediaTime();
+     
+     uint64_t bytesPerSecond = (bytes - _lastBytes) / (time - _lastTime);
+     
+     _lastBytes = bytes;
+     _lastTime = time;
+ 
+ 
+ @param type traffic types
+ @return bytes counter.
+ */
+- (uint64_t)getNetworkTrafficBytes:(YYNetworkTrafficType)types;
+
+
 #pragma mark - Disk Space
 ///=============================================================================
 /// @name Disk Space
--- YYKit/Base/UIKit/UIDevice+YYAdd.m
+++ YYKit/Base/UIKit/UIDevice+YYAdd.m
@@ -17,6 +17,7 @@
 #include <mach/mach.h>
 #include <arpa/inet.h>
 #include <ifaddrs.h>
+#include <libkern/OSAtomic.h>
 #import "YYKitMacro.h"
 #import "NSString+YYAdd.h"
 
@@ -134,6 +135,93 @@ YYSYNTH_DUMMY_CLASS(UIDevice_YYAdd)
     }
     freeifaddrs(addrs);
     return address;
+}
+
+
+typedef struct {
+    uint64_t en_in;
+    uint64_t en_out;
+    uint64_t pdp_ip_in;
+    uint64_t pdp_ip_out;
+    uint64_t awdl_in;
+    uint64_t awdl_out;
+} yy_net_interface_counter;
+
+
+static uint64_t yy_net_counter_add(uint64_t counter, uint64_t bytes) {
+    if (bytes < (counter % 0xFFFFFFFF)) {
+        counter += 0xFFFFFFFF - (counter % 0xFFFFFFFF);
+        counter += bytes;
+    } else {
+        counter = bytes;
+    }
+    return counter;
+}
+
+static uint64_t yy_net_counter_get_by_type(yy_net_interface_counter *counter, YYNetworkTrafficType type) {
+    uint64_t bytes = 0;
+    if (type & YYNetworkTrafficTypeWWANSent) bytes += counter->pdp_ip_out;
+    if (type & YYNetworkTrafficTypeWWANReceived) bytes += counter->pdp_ip_in;
+    if (type & YYNetworkTrafficTypeWIFISent) bytes += counter->en_out;
+    if (type & YYNetworkTrafficTypeWIFIReceived) bytes += counter->en_in;
+    if (type & YYNetworkTrafficTypeAWDLSent) bytes += counter->awdl_out;
+    if (type & YYNetworkTrafficTypeAWDLReceived) bytes += counter->awdl_in;
+    return bytes;
+}
+
+static yy_net_interface_counter yy_get_net_interface_counter() {
+    static OSSpinLock lock = OS_SPINLOCK_INIT;
+    static NSMutableDictionary *sharedInCounters;
+    static NSMutableDictionary *sharedOutCounters;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInCounters = [NSMutableDictionary new];
+        sharedOutCounters = [NSMutableDictionary new];
+    });
+    
+    yy_net_interface_counter counter = {0};
+    struct ifaddrs *addrs;
+    const struct ifaddrs *cursor;
+    if (getifaddrs(&addrs) == 0) {
+        cursor = addrs;
+        OSSpinLockLock(&lock);
+        while (cursor) {
+            if (cursor->ifa_addr->sa_family == AF_LINK) {
+                const struct if_data *data = cursor->ifa_data;
+                NSString *name = cursor->ifa_name ? [NSString stringWithUTF8String:cursor->ifa_name] : nil;
+                if (name) {
+                    uint64_t counter_in = ((NSNumber *)sharedInCounters[name]).unsignedLongLongValue;
+                    counter_in = yy_net_counter_add(counter_in, data->ifi_ibytes);
+                    sharedInCounters[name] = @(counter_in);
+                    
+                    uint64_t counter_out = ((NSNumber *)sharedOutCounters[name]).unsignedLongLongValue;
+                    counter_out = yy_net_counter_add(counter_out, data->ifi_obytes);
+                    sharedOutCounters[name] = @(counter_out);
+                    
+                    if ([name hasPrefix:@"en"]) {
+                        counter.en_in += counter_in;
+                        counter.en_out += counter_out;
+                    } else if ([name hasPrefix:@"awdl"]) {
+                        counter.awdl_in += counter_in;
+                        counter.awdl_out += counter_out;
+                    } else if ([name hasPrefix:@"pdp_ip"]) {
+                        counter.pdp_ip_in += counter_in;
+                        counter.pdp_ip_out += counter_out;
+                    }
+                }
+            }
+            cursor = cursor->ifa_next;
+        }
+        OSSpinLockUnlock(&lock);
+        freeifaddrs(addrs);
+    }
+    
+    return counter;
+}
+
+- (uint64_t)getNetworkTrafficBytes:(YYNetworkTrafficType)types {
+    yy_net_interface_counter counter = yy_get_net_interface_counter();
+    return yy_net_counter_get_by_type(&counter, types);
 }
 
 - (NSString *)machineModel {
