0f3acc59c8a60228e3077cebb6edf301f7469ff3
ibireme <ibireme@gmail.com>
2016-04-02 12:55:06 +0800

    fix potential crash in iOS7

--- YYKit/Text/Component/YYTextEffectWindow.m
+++ YYKit/Text/Component/YYTextEffectWindow.m
@@ -401,7 +401,11 @@
         }
     }
     CGPoint center = [dot convertPoint:CGPointMake(CGRectGetWidth(dot.frame) / 2, CGRectGetHeight(dot.frame) / 2) toViewOrWindow:self];
-    dot.mirror.center = center;
+    if (isnan(center.x) || isnan(center.y) || isinf(center.x) || isinf(center.y)) {
+        dot.mirror.hidden = YES;
+    } else {
+        dot.mirror.center = center;
+    }
 }
 
 - (void)showSelectionDot:(YYTextSelectionView *)selection {
